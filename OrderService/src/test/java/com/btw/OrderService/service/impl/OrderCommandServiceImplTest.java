5750eae95388007e9c104e886adebf536ede2fd1
yueyi <elvis.yue@i9i8.com>
2021-03-26 10:45:21 +0800

    创建项目

---
+++ OrderService/src/test/java/com/btw/OrderService/service/impl/OrderCommandServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.btw.OrderService.service.impl;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+class OrderCommandServiceImplTest {
+
+    @Test
+    void addOrder() {
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+
+    @Test
+    void getUserAllOrder() {
+    }
+
+    @Test
+    void getAllOrder() {
+    }
+}
